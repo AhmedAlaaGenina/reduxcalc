4bb4219991fb0aaf5426d5008399a66e994234c3
ahmedalaa4080 <32454614+ahmedalaa4080@users.noreply.github.com>
2020-08-21 10:54:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class App extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <button onClick={this.props.increase}>+</button>
+        <div>{this.props.count}</div>
+        <button onClick={this.props.decrease}>-</button>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { count: state.count };
+}
+const action1 = {
+  type: "INCREASE",
+};
+const action2 = {
+  type: "DECREASE",
+};
+function mapDispatchToProps(dispatch) {
+  return {
+    increase: () => dispatch(action1),
+    decrease: () => dispatch(action2),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+const iniState = {
+  count: 0,
+};
+
+const reducer = (state = iniState, action) => {
+  if (action.type === "INCREASE") {
+    return { count: state.count + 1 };
+  } else if (action.type === "DECREASE") {
+    return { count: state.count - 1 };
+  }
+  return state;
+}; 
+
+export default reducer;
